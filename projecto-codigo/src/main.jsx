db1feb65e8fb967dd2158b60d5503c1672de0e58
VictorTru <vtrujillocharalla@gmail.com>
2023-04-29 23:41:48 -0500

    Components de Header y Footer

--- projecto-codigo/src/main.jsx
+++ projecto-codigo/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Precio from './pages/Precio'
 import './index.css'
+import Header from './components/Header'
+import  Footer  from './components/Footer'
+import Router from './router/Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Precio/>
+    <Router/>
   </React.StrictMode>,
 )
--- projecto-codigo/tailwind.config.js
+++ projecto-codigo/tailwind.config.js
@@ -3,12 +3,13 @@ module.exports = {
   content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
-      "./node_modules/tw-elements/dist/js/**/*.js"
+      "./node_modules/tw-elements/dist/js/**/*.js",
+      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
       extend: {},
   },
   darkMode: "class",
-  plugins: [require("tw-elements/dist/plugin.cjs")]
+  plugins: [require("tw-elements/dist/plugin.cjs"), require('flowbite/plugin')]
   }
 
